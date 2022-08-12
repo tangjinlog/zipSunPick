77fa94e07f5168b972cdc1831cc7d325a55ed741
tangjinlog <lkg70007@gmail.com>
2022-08-12 21:54:00 +0900

    .gitignore 추가

--- server/src/config/default.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-  db: {
-    mongoUrl: "mongodb+srv://tanglog:lkg841235*@tang.qgpop.mongodb.net/?retryWrites=true&w=majority"
-  },
-  port: 3001,
-  client: "http://localhost:3000"
-}
