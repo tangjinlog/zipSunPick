efd5c53185feb5ba0af00d02cc0db54dac23c904
tangjinlog <lkg70007@gmail.com>
2022-08-19 19:41:33 +0900

    스키마 생성 , 모델 추가, 다큐먼트 생성 + data 세이브

--- server/src/models/sample/schema.js
+++ server/src/models/sample/schema.js
@@ -2,9 +2,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const ChatSchema = new Schema({
-  userId: { type: String, required: true },
+  userId: { type: String },
   text: { type: String, createdAt: { type: Date, default: Date.now, immutable: true }},
 },
 { versionKey: false })
 
 module.exports = mongoose.model('Chat', ChatSchema);
+
