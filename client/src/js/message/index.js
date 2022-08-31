f30a402fef98c828df9793ca7c95a4e9b523298a
tangjinlog <lkg70007@gmail.com>
2022-08-31 15:42:17 +0900

    댓글기능 : 한 번에 한 개의 reply-form이 열리도록 조건 설정

--- client/src/js/message/index.js
+++ client/src/js/message/index.js
@@ -23,6 +23,7 @@ export default function message() {
     userLocation.setAttribute('class', 'user-location');
     msgContent.setAttribute('class', 'msg-content');
     replyBtn.setAttribute('class', 'reply-btn');
+    chatLog.setAttribute('aria-expanded', false);
     replyBtn.innerText = '답글쓰기';
 
     userId.innerText = '';
@@ -36,47 +37,109 @@ export default function message() {
 
     chatLog.scrollTop = chatLog.scrollHeight - chatLog.clientHeight;
 
+    // console.log(data.comment);
+    /* reply form add */
+    // if( data.comment) {
+    // console.log(data.id);
+      
+    // }
     
+
+
     replyBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      const replyForm = document.createElement('form');
-      const replyMsg = document.createElement('input');
-      const replyFormBtn = document.createElement('input');
-      
+      if(document.querySelector('.reply-form') == null || chatLog.getAttribute('aria-expanded') == 'false') {
+        chatLog.setAttribute('aria-expanded', true);
+
+        const replyForm = document.createElement('form');
+        const replyMsg = document.createElement('input');
+        const replyFormBtn = document.createElement('input');
+        
   
-      replyForm.setAttribute('class', 'reply-form flex-r');
-      replyMsg.setAttribute('id', 'reply-msg');
-      replyMsg.setAttribute('type', 'text');
-      replyFormBtn.setAttribute('id', 'reply-form-btn');
-      replyFormBtn.setAttribute('type', 'submit');
-      replyForm.append(replyMsg,replyFormBtn);
-      console.log(replyForm)
-      msgContentBox.append(replyForm);
-      
-      replyFormBtn.addEventListener('click', (e) => {
-
-        socket.emit('SEND_REPLY', { 
-          // userId: //mongodb에서 받아올 userid,
-          // userImg
-          // userLocation
-          comment_author: 'tangjin',
-          comment_text: replyMsg.value,
+    
+        replyForm.setAttribute('class', 'reply-form flex-r');
+        replyMsg.setAttribute('id', 'reply-msg');
+        replyMsg.setAttribute('type', 'text');
+        replyFormBtn.setAttribute('id', 'reply-form-btn');
+        replyFormBtn.setAttribute('type', 'submit');
+        replyFormBtn.setAttribute('value', '등록');
+        replyForm.append(replyMsg,replyFormBtn);
+        msgContentBox.append(replyForm);
+        
+        replyFormBtn.addEventListener('click', (e) => {
+          e.preventDefault();
+          
+          replyForm.style.display = 'none';
+          console.log(replyBtn)
+          chatLog.setAttribute('aria-expanded', false);
+          
+          const replyCon = document.createElement('div');
+          const replyImgBox = document.createElement('div')
+          const replyUserImg = document.createElement('span')
+          const replyContentBox = document.createElement('div')
+          const replyUserId = document.createElement('span')
+          const replyUserLocation = document.createElement('span')
+          const replyContent = document.createElement('p')
+          
+          //클릭이 일어난 부모 node 찾기
+          const msgContentBox = e.currentTarget.parentNode.parentNode;
+  
+          replyCon.setAttribute('class', 'reply msg-con flex-r');
+          replyImgBox.setAttribute('class', 'msg-img-box');
+          replyUserImg.setAttribute('class', 'user-img');
+          replyContentBox.setAttribute('class', 'msg-content-box');
+          replyUserId.setAttribute('class', 'user-id');
+          replyUserLocation.setAttribute('class', 'user-location');
+          replyContent.setAttribute('class', 'msg-content');
+          
+          replyContent.innerText = data.comment_text
+          
+          msgContentBox.append(replyCon);
+          replyCon.append(replyImgBox, replyContentBox);
+          replyImgBox.append(replyUserImg);
+          replyContentBox.append(replyUserId, replyUserLocation, replyContent);
+  
+          socket.emit('SEND_REPLY', { 
+            // userId: //mongodb에서 받아올 userid,
+            // userImg
+            // userLocation
+            comment_author: 'tangjin', 
+            comment_text: replyMsg.value,
+          })
         })
-      })
-
-      replyMsg.value = '';
+  
+        replyMsg.value = '';
+      }
     })
   }
+  /**
+   * 각각의 _id값이 있다. data.save 시.
+   * 
+   */
+  function createReply(data,e) {
+    
+  }
+
   /* preload */
   socket.on('preload', (data) => {
     createElem(data);
-    // reply();
+    if(data.comment_text) {
+      createReply(data);
+    }
   })
 
   socket.on('SEND_MESSAGE', (data) => {
     createElem(data);
     // reply();
   })
+
+  socket.on('SEND_REPLY', (data) => {
+    // createElem(data);
+    
+    createReply(data);
+
+    
+  })
+
 
   chatBtn.addEventListener('click', (e) => {
     e.preventDefault();
