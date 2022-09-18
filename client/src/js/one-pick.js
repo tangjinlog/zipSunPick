6e99863947cd2f1a6a961711eb772e7d4f5d4efe
tangjinlog <lkg70007@gmail.com>
2022-09-18 23:45:19 +0900

    animate keyframes 안됨 -> animationend - animate로 해결

--- client/src/js/one-pick.js
+++ client/src/js/one-pick.js
@@ -1,7 +1,10 @@
 /* Pick Animation */
 const pickCon1 = document.querySelector('.pick-1-con');
-
+const pickCon2 = document.querySelector('.pick-2-con');
+const pickCon3 = document.querySelector('.pick-3-con');
 let pickTiming = { duration: 200, iterations: 1, fill: 'forwards' };
+let pickTiming2 = { duration: 800, iterations: 1, fill: 'forwards', delay: 400 };
+let pickTiming3 = { duration: 800, iterations: 1, fill: 'forwards', delay: 600 };
 
 const pickBox1 = document.querySelector('.pick-1-box');
 pickBox1.innerHTML = ''
@@ -29,31 +32,35 @@ pickBox1.innerHTML = ''
 pickCon1.addEventListener('animationend', () => {
   const pickScale = [ {transform: 'scale(1)'}, {transform: 'scale(1.2)'} ];
   pickCon1.animate(pickScale, pickTiming);
+  const pickTranslate1 = [ {transform: 'translateY(4vh)', opacity: 0}, {transform: 'translateY(0)', opacity: 1} ];
+  const pickTranslate2 = [ {transform: 'translateY(4vh)', opacity: 0}, {transform: 'translateY(0)', opacity: 1} ];
+  pickCon2.animate(pickTranslate1, pickTiming2);
+  pickCon3.animate(pickTranslate2, pickTiming3);
 })
 
 function itemArrow() {
-  const itemListCon = document.querySelector('.item-list-con');
-  const leftArrow = document.querySelector('.arrow-left');
-  const rightArrow = document.querySelector('.arrow-right');
-  let itemList = document.getElementsByClassName('item-list');
-  let index = 0;
+    const itemListCon = document.querySelector('.item-list-con');
+    const leftArrow = document.querySelector('.arrow-left');
+    const rightArrow = document.querySelector('.arrow-right');
+    let itemList = document.getElementsByClassName('item-list');
+    let index = 0;
 
-  leftArrow.addEventListener('click', () => {
-    if( index == 0 ) {
-      index = itemList.length - 3; // 1
-    } else {
-      index--;
-    } 
-    itemListCon.style.marginLeft = '-' + index * 33.33 + '%';
-  })
+    leftArrow.addEventListener('click', () => {
+      if( index == 0 ) {
+        index = itemList.length - 3; // 1
+      } else {
+        index--;
+      } 
+      itemListCon.style.marginLeft = '-' + index * 33.33 + '%';
+    })
 
-  rightArrow.addEventListener('click', () => {
-    if( index == itemList.length - 3 ) {
-      index = 0; 
-    } else {
-      index++;
-    } 
-    itemListCon.style.marginLeft =  '-'+ index * 33.33 + '%';
-  })
-}
-itemArrow();
+    rightArrow.addEventListener('click', () => {
+      if( index == itemList.length - 3 ) {
+        index = 0; 
+      } else {
+        index++;
+      } 
+      itemListCon.style.marginLeft =  '-'+ index * 33.33 + '%';
+    })
+  }
+  itemArrow();
